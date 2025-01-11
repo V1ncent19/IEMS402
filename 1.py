0c570c28b8d16e3239e9f7715ba42b8b8c320f8e
v1ncent19 <v1ncent19@outlook.com>
2025-01-11 13:30:06 -0600

    HW concate, v1ncent19

--- 1.py
+++ 1.py
@@ -8,3 +8,23 @@ for i in range(1, 9):
     with open(f"HW{i}/HW{i}_assignment.pdf", "wb") as f:
         f.write(response.content)
     print(f"Downloaded HW{i}.pdf")
+
+
+# make a concatenated PDF file: HW{x}/HW{x}.pdf for x in 1 to 8, into PDF named "HWs.pdf"
+import PyPDF2
+
+pdf_writer = PyPDF2.PdfWriter()
+for i in range(1, 9):
+    with open(f"HW{i}/HW{i}.pdf", "rb") as f:
+        pdf_reader = PyPDF2.PdfReader(f)
+        for page in pdf_reader.pages:
+            pdf_writer.add_page(page)
+
+# Save the concatenated PDF to a HWs.pdf 
+
+with open("HWs.pdf", "wb") as output_pdf:
+    pdf_writer.write(output_pdf)
+    
+
+
+
